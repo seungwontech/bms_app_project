999c015fe342ce459bdd2257ca541341f3f1ef90
sw.lee <swlee173@gmail.com>
2024-02-23 17:14:45 +0900

    ...

--- src/main/java/com/ssproject/bms/book/controller/BookController.java
+++ src/main/java/com/ssproject/bms/book/controller/BookController.java
@@ -19,53 +19,16 @@ import java.net.URI;
 import java.util.List;
 
 @Controller
+@RequestMapping("/book")
 public class BookController {
 
-    @GetMapping("/book/list")
+    @GetMapping("/list")
     public String naverApiBookList(String searchText, Model model) {
-        String clientId = "";
-        String clientSecret = "";
-
-        searchText = "스프링부트";
-
-        URI uri = UriComponentsBuilder
-                .fromUriString("https://openapi.naver.com")
-                .path("/v1/search/book.json")
-                .queryParam("query", searchText)
-                .queryParam("display", 10)
-                .queryParam("start", 1)
-                .queryParam("sort", "sim")
-                .encode()
-                .build()
-                .toUri();
-
-        RequestEntity<Void> requestEntity = RequestEntity
-                .get(uri)
-                .header("X-Naver-Client-Id", clientId)
-                .header("X-Naver-Client-Secret", clientSecret)
-                .build();
-
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.exchange(requestEntity, String.class);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        NaverBookSearchResultApiVO naverBookSearchResultApiVO = null;
-        try {
-            naverBookSearchResultApiVO = objectMapper.readValue(responseEntity.getBody(), NaverBookSearchResultApiVO.class);
-        } catch (JsonMappingException e) {
-            e.printStackTrace();
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-
-        List<NaverBookApiVO> naverBookApiList = naverBookSearchResultApiVO.getItems();
-        model.addAttribute("naverBookApiList", naverBookApiList);
         return "book/book_naver_search_list.html";
     }
 
-    @GetMapping("/book/getNaverBookSearchList/{searchText}")
+    @GetMapping("/getNaverBookSearchList/{searchText}")
     public ResponseEntity<List<NaverBookApiVO>> getNaverBookSearchList(@PathVariable String searchText) {
-        System.out.println("searchText : "+searchText);
 
         String clientId = "";
         String clientSecret = "";
