8c583ca354f14969452b59f349e5fe21b9dbd6ee
sw.lee <swlee173@gmail.com>
2024-04-15 00:22:12 +0900

    CustomUserDetails 객체에 담아 반환, 현재 사용자 정보 (mberId) 조회 추가

--- src/main/java/com/ssproject/bms/member/service/MemberService.java
+++ src/main/java/com/ssproject/bms/member/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.ssproject.bms.member.service;
 
+import com.ssproject.bms.member.dto.CustomUserDetails;
 import com.ssproject.bms.member.dto.MemberDTO;
 import com.ssproject.bms.member.entity.AuthorEntity;
 import com.ssproject.bms.member.entity.MemberAuthorEntity;
@@ -8,17 +9,21 @@ import com.ssproject.bms.member.repository.AuthorRepository;
 import com.ssproject.bms.member.repository.MemberAuthorRepository;
 import com.ssproject.bms.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
-import java.util.Optional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -30,12 +35,18 @@ public class MemberService implements UserDetailsService {
     private final MemberAuthorRepository memberAuthorRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    /**
+     * 권한 정보 조회
+     *
+     * @param authorId
+     * @return
+     */
     public AuthorEntity getAuthorInfo(int authorId) {
         return authorRepository.findByAuthorId(authorId).orElseThrow();
     }
 
     /**
-     * 회원가입
+     * 회원가입 처리
      *
      * @param memberDTO
      */
@@ -54,22 +65,46 @@ public class MemberService implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String mberEmail) throws UsernameNotFoundException {
-
+    public CustomUserDetails loadUserByUsername(String mberEmail) throws UsernameNotFoundException {
+        // 이메일로 회원 정보 조회
         MemberEntity mberInfo = memberRepository.findByMberEmail(mberEmail)
                 .orElseThrow(() -> new UsernameNotFoundException("Email: " + mberEmail + " not found"));
-        return new org.springframework.security.core.userdetails.User(mberInfo.getMberEmail(),
-                mberInfo.getMberPw(), getAuthorities(mberInfo));
 
+        String username = mberInfo.getMberEmail();
+        String password = mberInfo.getMberPw();
+        int mberId = mberInfo.getMberId();
+
+        return new CustomUserDetails(username, password, mberId, getAuthorities(mberInfo));
     }
 
-    private static Collection<? extends GrantedAuthority> getAuthorities(MemberEntity mberInfo) {
-        String[] mberAuthors = mberInfo.getMemberAuthors()
-                .stream()
-                .map((author) -> author.getAuthorEntity().getAuthorNm())
-                .toArray(String[]::new);
-
-        Collection<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList(mberAuthors);
+    /**
+     * 회원 권한 조회
+     *
+     * @param mberInfo
+     * @return
+     */
+    private Collection<? extends GrantedAuthority> getAuthorities(MemberEntity mberInfo) {
+        List<GrantedAuthority> authorities = mberInfo.getMemberAuthors().stream()
+                .map(memberAuthor -> new SimpleGrantedAuthority(memberAuthor.getAuthorEntity().getAuthorNm()))
+                .collect(Collectors.toList());
         return authorities;
+    }
+
+    /**
+     * 현재 사용자 정보 조회
+     *
+     * @return
+     */
+    public Object getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof UserDetails) {
+                return ((CustomUserDetails) principal).getMberId();
+            } else if (principal instanceof OAuth2User) {
+                return ((OAuth2User) principal).getAttribute("mber_id");
+            }
+        }
+        return null;
     }
 }
